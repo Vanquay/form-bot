2dd0cfed194ad3669fbcf4159a2c0efa6c0da2f2
Vanquay <vanquay16@gmail.com>
2023-06-03 23:47:28 -0500

    chore: nodemon configs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
+import { Builder } from "selenium-webdriver";
+
+const url = process.env.SIGNUP_URL
+console.log(url)
+
 console.log('Workday sign up bot initialized');
+
+const driver = await new Builder().forBrowser('chrome').build()
+
+
